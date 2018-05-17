2ad1b73547891aba3922c7c8b2f7027e9c4ec876
anvesh kumar somesula <anvesh1891@gmail.com>
2018-05-17 15:21:13 +0530

    reverse

---
+++ anvesh/reverse_integer.cpp
@@ -0,0 +1,16 @@
+unsigned int Solution::reverse(unsigned int A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int nob=sizeof(A)*8;
+    int op=0;
+    for(int i=0;i<nob;i++){
+        int temp=A&(1<<i);
+        if(temp){
+            op=op|(1<<(nob-1)-i);
+        }
+        
+    }
+    return op;
+}
