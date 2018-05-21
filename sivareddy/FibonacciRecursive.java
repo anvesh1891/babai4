a3570ebfdbf8fa70a2eac78885149a2244ec4823
A1344370 <A1344370@bestbuy.com>
2018-05-21 14:46:21 +0530

    Fibonacci Recursive Solution

---
+++ sivareddy/FibonacciRecursive.java
@@ -0,0 +1,13 @@
+class FibonacciRecursive {
+  public static int fib(int n)
+  {
+    if(n==1||n==2)
+      return 1;
+    return fib(n-1)+fib(n-2);
+  }
+  public static void main(String[] args) {
+    int n=5;
+    System.out.println("Fiboncci of nth number is:"+fib(n));
+
+  }
+}
